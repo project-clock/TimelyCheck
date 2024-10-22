4aac5fb949f0a147763b1db14761872bda3bdbe1
SandeepKumar Rayala <sandeepkumarrayala@gmail.com>
2024-10-22 20:34:18 +0530

    intial commit backend
    Co-Authored-By: SandeepKumar Rayala <59500898+sandeep9121@users.noreply.github.com>

---
+++ timelycheck/src/main/java/com/americinn/timelycheck/timelycheck/TimelycheckApplication.java
@@ -0,0 +1,13 @@
+package com.americinn.timelycheck.timelycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelycheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelycheckApplication.class, args);
+	}
+
+}
---
+++ timelycheck/src/test/java/com/americinn/timelycheck/timelycheck/TimelycheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.americinn.timelycheck.timelycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelycheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
