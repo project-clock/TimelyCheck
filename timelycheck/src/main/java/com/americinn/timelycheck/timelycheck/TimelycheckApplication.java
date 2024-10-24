57ac791bf7eba952e25a3dbbaaaa0786408ea4ff
SandeepKumar Rayala <sandeepkumarrayala@gmail.com>
2024-10-25 01:28:10 +0530

    initial commit

--- timelycheck/src/main/java/com/americinn/timelycheck/timelycheck/TimelycheckApplication.java
+++ timelycheck/src/main/java/com/americinn/timelycheck/timelycheck/TimelycheckApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TimelycheckApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TimelycheckApplication.class, args);
 	}
 
